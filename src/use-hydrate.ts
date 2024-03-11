c26effac985193bbc7d68cc30c33d0ba19a4bca2
Samuel Richard Magny <samuelint@gmail.com>
2024-03-11 09:03:25 -0400

    fix: typing

--- src/use-hydrate.ts
+++ src/use-hydrate.ts
@@ -3,31 +3,22 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 
-export interface UseHydrateProps<
-  TValue,
-  TAction extends ActionCreatorWithPayload<TValue>,
-  TSelectorValue,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  TSelector extends (state: any) => TSelectorValue
-  > {
-  initial: TValue
-  action: TAction
-  selector: TSelector
-}
-
 export function useHydrate<
-  TValue,
-  TAction extends ActionCreatorWithPayload<TValue> = ActionCreatorWithPayload<TValue>,
-  TSelectorValue = TValue,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  TSelector extends (state: any) => TSelectorValue = () => TSelectorValue
-  >({ initial, action, selector }: UseHydrateProps<TValue, TAction, TSelectorValue, TSelector>) {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch((action(initial)));
-  // Only at first render
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  TInitialValue, 
+  TState,
+  TSelectedValue = unknown,
+  TAction extends ActionCreatorWithPayload<TInitialValue> = ActionCreatorWithPayload<TInitialValue>, 
+  
+>
+    (initial: TInitialValue,
+      action: TAction,
+      selector: (state: TState) => TSelectedValue): TSelectedValue {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(action(initial));
+    // Only at first render
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  return useSelector(selector);
+    return useSelector<TState, TSelectedValue>(selector);
 }
