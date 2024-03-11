6db07f5db58d170ec0db7d8763deb2feb6d58295
Samuel Richard Magny <samuelint@gmail.com>
2024-03-10 22:00:14 -0400

    feat: allow selector different return type

--- src/use-hydrate.ts
+++ src/use-hydrate.ts
@@ -6,8 +6,9 @@ import { useDispatch, useSelector } from 'react-redux';
 export interface UseHydrateProps<
   TValue,
   TAction extends ActionCreatorWithPayload<TValue>,
+  TSelectorValue,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  TSelector extends (state: any) => TValue
+  TSelector extends (state: any) => TSelectorValue
   > {
   initial: TValue
   action: TAction
@@ -17,9 +18,10 @@ export interface UseHydrateProps<
 export function useHydrate<
   TValue,
   TAction extends ActionCreatorWithPayload<TValue> = ActionCreatorWithPayload<TValue>,
+  TSelectorValue = TValue,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  TSelector extends (state: any) => TValue = () => TValue
-  >({ initial, action, selector }: UseHydrateProps<TValue, TAction, TSelector>) {
+  TSelector extends (state: any) => TSelectorValue = () => TSelectorValue
+  >({ initial, action, selector }: UseHydrateProps<TValue, TAction, TSelectorValue, TSelector>) {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch((action(initial)));
